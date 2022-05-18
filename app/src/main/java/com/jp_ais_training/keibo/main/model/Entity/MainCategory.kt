a45b5cf3fa8ccac654c4dec47e4688a888f5a27c
AIS-NB-102\NB-102 <xogns6515@gmail.com>
2022-05-18 18:00:34 +0900

    db

---
+++ app/src/main/java/com/jp_ais_training/keibo/main/model/Entity/MainCategory.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.model.Entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "MainCategory")
+data class MainCategory(
+    @PrimaryKey val main_category_id: Int,
+    @ColumnInfo(name = "main_category_name") val main_category_name: String,
+    @ColumnInfo(name = "expense_type") val expense_type: String
+)
---
+++ app/src/main/java/com/jp_ais_training/keibo/main/model/example.kt
@@ -0,0 +1,29 @@
+package com.jp_ais_training.keibo.main.model
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.model.AppDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+
+class ExActivity : AppCompatActivity() {
+
+    private lateinit var DB: AppDatabase
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        DB = AppDatabase.getInstance(this)!!
+
+        //사용시 CoroutineScope 필요
+        val start = CoroutineScope(Dispatchers.IO).async {
+            var data = DB.dao().loadEI("2022-05-17")
+        }
+
+
+    }
+
+}

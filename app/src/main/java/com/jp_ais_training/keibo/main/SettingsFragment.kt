5e34940c96db010cae485664fa48468dac2f4abc
doyou1 <tla149@daum.net>
2022-05-24 12:06:23 +0900

    [Setting Fragment Layout Design] build.gradle - DataBinding 추가 dimens.xml - SettingsFragment 관련 dimen 추가 fragment_settings.xml - 토글(switch)과 토글 라벨 레이아웃 추가 SettingsFragment.kt - DataBinding & Switch 변경 이벤트 추가

--- app/src/main/java/com/jp_ais_training/keibo/main/SettingsFragment.kt
+++ app/src/main/java/com/jp_ais_training/keibo/main/SettingsFragment.kt
@@ -1,60 +1,75 @@
 package com.jp_ais_training.keibo.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.jp_ais_training.keibo.R
+import com.jp_ais_training.keibo.databinding.FragmentSettingsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val TAG = this::class.java.simpleName.toString()
+    private lateinit var binding:FragmentSettingsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_settings, container, false)
+
+        Log.d(TAG, "onCreateView")
+
+        setClickEvent()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun setClickEvent() {
+        binding.swiAll.setOnCheckedChangeListener { compoundButton, isChecked ->
+            Log.d(TAG, "swiAll: $isChecked")
+        }
+
+        binding.swiAlarm1.setOnCheckedChangeListener { compoundButton, isChecked ->
+            Log.d(TAG, "swiAlarm1: $isChecked")
+        }
+
+        binding.swiAlarm2.setOnCheckedChangeListener { compoundButton, isChecked ->
+            Log.d(TAG, "swiAlarm2: $isChecked")
+        }
+
+        binding.swiAlarm3.setOnCheckedChangeListener { compoundButton, isChecked ->
+            Log.d(TAG, "swiAlarm3: $isChecked")
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
 }

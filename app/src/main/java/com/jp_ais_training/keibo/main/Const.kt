6570cb941cfd6f0770fa646cd6ca28aa6da3c994
doyou1 <tla149@daum.net>
2022-05-25 18:05:42 +0900

    [가계부 기입 요청 알람] - Const.kt : 공통 상수 추가 - KeiboApplication.kt : 알림관련 로직 (매번실행 -> 최초 앱기동시만 실행), 공통 상수 활용 - KinyuNotiReceiver.kt : KeiboApplication 코드 변화에 따른 로직 변경, 공통 상수 활용 - NotificationUtil.kt : 공통 상수 활용

--- app/src/main/java/com/jp_ais_training/keibo/main/Const.kt
+++ app/src/main/java/com/jp_ais_training/keibo/main/Const.kt
@@ -1,11 +1,37 @@
 package com.jp_ais_training.keibo.main
 
+import android.content.SharedPreferences
+
 // 애플리케이션에서 활용할 공통 상수
 object Const {
 
-    // 가계부 기입 요청 알림
+    // 가계부 기입 요청 알림 관련 상수
     val KINYU_CHANNEL_ID = "KINYU"
     val KINYU_CHANNEL_NAME = "KINYU CHANNEL"
 
     val KINYU_NOTIFICATION = -777
+    val IS_INIT_KINYU_NOTI = "isInitKinyuNoti"
+    val NOTI_KEY = "Noti"
+    val FIX_EXPENSE_NOTI_KEY = "FixExpenseNoti"
+    val KINYU_NOTI_KEY = "KinyuNoti"
+    val COMPARISON_EXPENSE_NOTI_KEY = "ComparisonExpense"
+
+    val KINYU_NOTI_CONTENT_TITLE = "家計簿記入要請通知"
+    val KINYU_NOTI_CONTENT_TEXT = "家計簿記入が届いてありません。記入してください!"
+
+    val KINYU_MAIN_ACTIVITY_EXTRA_TYPE = "type"
+    val KINYU_MAIN_ACTIVITY_EXTRA_YEAR = "year"
+    val KINYU_MAIN_ACTIVITY_EXTRA_MONTH = "month"
+    val KINYU_MAIN_ACTIVITY_EXTRA_DAY = "day"
+
+    val KINYU_MAIN_ACTIVITY_PENDING_INTENT_REQUEST_CODE = 0
+    val KINYU_MAIN_ACTIVITY_PENDING_INTENT_FLAGS = 0
+
+    val KINYU_NOTI_RECEIVER_PENDING_INTENT_REQUEST_CODE = 0
+    val KINYU_NOTI_RECEIVER_PENDING_INTENT_FLAGS = 0
+
+    val KINYU_NOTI_HOUR_OF_DAY = 21
+    val KINYU_NOTI_MINUTE = 0
+    val KINYU_NOTI_SECOND = 0
+    val KINYU_NOTI_MILLISECOND = 0
 }
--- app/src/main/java/com/jp_ais_training/keibo/main/notireceiver/KinyuNotiReceiver.kt
+++ app/src/main/java/com/jp_ais_training/keibo/main/notireceiver/KinyuNotiReceiver.kt
@@ -4,7 +4,8 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.jp_ais_training.keibo.R
@@ -13,7 +14,6 @@ import com.jp_ais_training.keibo.main.MainActivity
 import com.jp_ais_training.keibo.main.model.AppDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import java.util.*
 
@@ -21,72 +21,68 @@ class KinyuNotiReceiver: BroadcastReceiver() {
 
     private val TAG = this::class.java.simpleName.toString()
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onReceive(context: Context?, intent: Intent?) {
         intent?.let { it ->
             // 홈화면 -> 상세페이지이므로
             val intent = Intent(context, MainActivity::class.java)
 
-            val year = it.getStringExtra("year")
-            val month = it.getStringExtra("month").let {
-                if (it != null) {
-                    if (it.toInt() > 10) {
-                        it
-                    } else {
-                        "0$it"
-                    }
+            val calendar = Calendar.getInstance()
+
+            val year = calendar.get(Calendar.YEAR)
+            val month = (calendar.get(Calendar.MONTH) + 1).let {
+                if (it > 10) {
+                    it
                 } else {
-                    "00"
+                    "0$it"
                 }
             }
-            val day = it.getStringExtra("day").let {
-                if (it != null) {
-                    if (it.toInt() > 10) {
-                        it
-                    } else {
-                        "0$it"
-                    }
+            val day = calendar.get(Calendar.DAY_OF_MONTH).let {
+                if (it > 10) {
+                    it
                 } else {
-                    "00"
-                }
-            }
-
-            val DB = AppDatabase.getInstance(context!!)!!
-
-            CoroutineScope(Dispatchers.IO).launch {
-                // $year-$month-$day : YYYY-MM-DD
-                val fixEI = DB.dao().loadFixEI("$year-$month-$day")
-                val flexEI = DB.dao().loadFlexEI("$year-$month-$day")
-                val fixII = DB.dao().loadFixII("$year-$month-$day")
-                val flexII = DB.dao().loadFlexII("$year-$month-$day")
-
-                // 고정 지출, 변동 지출, 고정 수입, 변동 수입 모두 비어있다면,
-                if (fixEI.isEmpty() && flexEI.isEmpty() && fixII.isEmpty() && flexII.isEmpty()) {
-                    intent.flags = (Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                    intent.putExtra("type", Const.KINYU_NOTIFICATION)
-                    intent.putExtra("year", year)
-                    intent.putExtra("month", month)
-                    intent.putExtra("day", day)
-
-                    val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
-
-                    val contentTitle = "家計簿記入要請通知"
-                    val contentText = "$year-$month-$day\n家計簿記入が届いてありません。記入してください!"
-
-                    val builder = NotificationCompat.Builder(context!!, Const.KINYU_CHANNEL_ID)
-                        .setSmallIcon(R.drawable.ic_launcher_background)
-                        .setContentTitle(contentTitle)
-                        .setContentText(contentText)
-                        .setAutoCancel(true)
-                        .setDefaults(NotificationCompat.DEFAULT_ALL)
-                        .setPriority(NotificationCompat.PRIORITY_HIGH)
-                        .setContentIntent(pendingIntent)
-
-                    val notificationManagerCompat = NotificationManagerCompat.from(context!!)
-                    notificationManagerCompat.notify(0, builder.build())
+                    "0$it"
                 }
             }
 
 
+            if (month != "00" && day != "00") { // 문제없는 날짜 값일 경우
+                val DB = AppDatabase.getInstance(context!!)!!
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    // $year-$month-$day : YYYY-MM-DD
+                    val fixEI = DB.dao().loadFixEI("$year-$month-$day")
+                    val flexEI = DB.dao().loadFlexEI("$year-$month-$day")
+                    val fixII = DB.dao().loadFixII("$year-$month-$day")
+                    val flexII = DB.dao().loadFlexII("$year-$month-$day")
+
+                    // 고정 지출, 변동 지출, 고정 수입, 변동 수입 모두 비어있다면,
+                    if (fixEI.isEmpty() && flexEI.isEmpty() && fixII.isEmpty() && flexII.isEmpty()) {
+                        intent.flags = (Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        intent.putExtra(Const.KINYU_MAIN_ACTIVITY_EXTRA_TYPE, Const.KINYU_NOTIFICATION)
+                        intent.putExtra(Const.KINYU_MAIN_ACTIVITY_EXTRA_YEAR, year)
+                        intent.putExtra(Const.KINYU_MAIN_ACTIVITY_EXTRA_MONTH, month)
+                        intent.putExtra(Const.KINYU_MAIN_ACTIVITY_EXTRA_DAY, day)
+
+                        val pendingIntent = PendingIntent.getActivity(context, Const.KINYU_MAIN_ACTIVITY_PENDING_INTENT_REQUEST_CODE, intent, Const.KINYU_MAIN_ACTIVITY_PENDING_INTENT_FLAGS)
+
+                        val contentTitle = Const.KINYU_NOTI_CONTENT_TITLE
+                        val contentText = "$year-$month-$day\n${Const.KINYU_NOTI_CONTENT_TEXT}"
+
+                        val builder = NotificationCompat.Builder(context!!, Const.KINYU_CHANNEL_ID)
+                            .setSmallIcon(R.drawable.ic_launcher_background)
+                            .setContentTitle(contentTitle)
+                            .setContentText(contentText)
+                            .setAutoCancel(true)
+                            .setDefaults(NotificationCompat.DEFAULT_ALL)
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                            .setContentIntent(pendingIntent)
+
+                        val notificationManagerCompat = NotificationManagerCompat.from(context!!)
+                        notificationManagerCompat.notify(0, builder.build())
+                    }
+                }
+            }
         }
     }
 }

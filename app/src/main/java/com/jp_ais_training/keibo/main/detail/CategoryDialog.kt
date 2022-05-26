d96c275fb82fec5fef05aea012381b0ccfb2ea59
AIS-NB-103\AIS-N <xixxaxon@gmail.com>
2022-05-26 16:05:25 +0900

    카테고리 작업 임시저장

--- app/src/main/java/com/jp_ais_training/keibo/main/detail/CategoryDialog.kt
+++ app/src/main/java/com/jp_ais_training/keibo/main/detail/CategoryDialog.kt
@@ -3,18 +3,49 @@ package com.jp_ais_training.keibo.main.detail
 import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
+import com.jp_ais_training.keibo.R
 
 
 class CategoryDialog(context: Context) {
 
     val ctx = context
 
+    private fun displayAlterDialog(
+        context: Context,
+        title: String,
+        body: String,
+        dialogActions: MutableList<DialogAction>
+    ) {
+        var inflater = LayoutInflater.from(context)
+
+        val params = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1F
+        )
+        val inflatedLayout = inflater.inflate(R.layout.custom_alert_dialog,null)
+        val dialog = AlertDialog.Builder(context).create()
+        dialog.setView(inflatedLayout)
+
+        inflatedLayout.findViewById<TextView>(R.id.title).text = title
+        inflatedLayout.findViewById<TextView>(R.id.body).text = body
+        val buttonContainer = inflatedLayout.findViewById<LinearLayout>(R.id.buttonContainer)
+
+        for (dialogActions)
+    }
+
+    fun callSubCategory()
+
     fun callMainCategory() {
         val builder = AlertDialog.Builder(ctx)
 
+
+
         builder.setCancelable(true)
         builder.setTitle("メインカテゴリ")
+
 
         builder.setItems(
             arrayOf<CharSequence>("button 1", "button 2", "button 3", "button 4")
@@ -26,7 +57,10 @@ class CategoryDialog(context: Context) {
                 3 -> Toast.makeText(ctx, "clicked 4", Toast.LENGTH_SHORT).show()
             }
         }
+
         builder.create().show()
     }
 
 }
+
+class DialogAction(var text: String, var runnable: Runnable)

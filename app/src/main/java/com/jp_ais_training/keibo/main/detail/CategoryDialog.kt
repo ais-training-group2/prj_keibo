1af25b21a323f02c88dc1598b177b9c059d80abb
AIS-NB-103\AIS-N <xixxaxon@gmail.com>
2022-05-27 14:45:06 +0900

    카테고리 데이터 로드 및 UI 쓰레드 적용

--- app/src/main/java/com/jp_ais_training/keibo/main/detail/CategoryDialog.kt
+++ app/src/main/java/com/jp_ais_training/keibo/main/detail/CategoryDialog.kt
@@ -3,6 +3,8 @@ package com.jp_ais_training.keibo.main.detail
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
+import android.os.Build
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +12,8 @@ import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import androidx.core.view.children
 import androidx.core.view.setPadding
 import androidx.fragment.app.Fragment
@@ -23,59 +27,72 @@ class CategoryDialog(private val activity: Activity) {
 
     val app = activity.application as MyApplication
 
+
     private fun displayCategoryDialog(
         context: Context,
         title: String,
         body: String,
         dialogActions: MutableList<DialogAction>
     ) {
-        var inflater = LayoutInflater.from(context)
+        ContextCompat.getMainExecutor(context).execute {
 
-        val params = LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-        val inflatedLayout = inflater.inflate(R.layout.custom_alert_dialog, null)
-        val dialog = AlertDialog.Builder(context).create()
-        dialog.setView(inflatedLayout)
-        println("Check")
-        inflatedLayout.findViewById<TextView>(R.id.title).text = title
-        inflatedLayout.findViewById<TextView>(R.id.body).text = body
-        val buttonContainer = inflatedLayout.findViewById<LinearLayout>(R.id.buttonContainer)
+            var inflater = LayoutInflater.from(context)
 
+            val params = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            val inflatedLayout = inflater.inflate(R.layout.custom_alert_dialog, null)
+            val dialog = AlertDialog.Builder(context).create()
+            dialog.setView(inflatedLayout)
+            inflatedLayout.findViewById<TextView>(R.id.title).text = title
+            inflatedLayout.findViewById<TextView>(R.id.body).text = body
+            val buttonContainer = inflatedLayout.findViewById<LinearLayout>(R.id.buttonContainer)
 
-        for (i in 0 until dialogActions.size / 3) {
-            val dialogActionListLimit3 = ArrayList<DialogAction>()
-            for (j in 0..2) {
-                if (i * 3 + j < dialogActions.size)
-                    dialogActionListLimit3.add(dialogActions.elementAt(i * 3 + j))
-                else
-                    break
+            for (i in 0 until dialogActions.size / 3) {
+                val dialogActionListLimit3 = ArrayList<DialogAction>()
+                for (j in 0..2) {
+                    if (i * 3 + j < dialogActions.size)
+                        dialogActionListLimit3.add(dialogActions.elementAt(i * 3 + j))
+                    else
+                        break
+                }
+
+                var buttonRow: LinearLayout =
+                    inflater.inflate(R.layout.custom_alert_dialog_button, null) as LinearLayout
+                buttonRow.gravity=Gravity.CENTER
+                var buttons = buttonRow.children as Sequence<Button>
+
+                //button.elementAt(i)
+                for (i in 0 until buttons.count()) {
+                    buttons.elementAt(i).text = dialogActionListLimit3.elementAt(i).text
+                    buttons.elementAt(i).setOnClickListener(View.OnClickListener {
+                        dialogActionListLimit3.elementAt(i).runnable.run()
+                        dialog.dismiss()
+                    })
+                }
+                buttonContainer.addView(buttonRow, params)
             }
-
-            var buttonRow: LinearLayout =
-                inflater.inflate(R.layout.custom_alert_dialog_button, null) as LinearLayout
-            var buttons = buttonRow.children as Sequence<Button>
-
-            //button.elementAt(i)
-            for (i in 0 until buttons.count()) {
-                buttons.elementAt(i).text = dialogActionListLimit3.elementAt(i).text
-                buttons.elementAt(i).setOnClickListener(View.OnClickListener {
-                    dialogActionListLimit3.elementAt(i).runnable.run()
-                    dialog.dismiss()
-                })
-            }
-            buttonContainer.addView(buttonRow, params)
+            dialog.show()
         }
-        dialog.show()
     }
 
     fun callSubCategory() {
         val dialogActions = mutableListOf<DialogAction>()
-        dialogActions.add(DialogAction("a", Runnable {}))
-        dialogActions.add(DialogAction("b", Runnable {}))
-        dialogActions.add(DialogAction("c", Runnable {}))
-        dialogActions.add(DialogAction("a", Runnable {}))
-        dialogActions.add(DialogAction("b", Runnable {}))
+        CoroutineScope(Dispatchers.IO).launch {
+            runBlocking {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val mainCategoryList = app.db.loadSubCategory()
+                    mainCategoryList.forEach { mainCategory ->
+                        dialogActions.add(DialogAction(mainCategory.main_category_name, Runnable {
+                            //액티비티에 네임 보내기 필요에 따라서 id도
+                            // 콜 서브 카테고리 다이얼 로크
+                        }))
+                    }
+                }.join()
+                displayCategoryDialog(activity, "メインカテゴリ", "メインカテゴリを選択してください。", dialogActions)
+
+            }
+        }
 
         displayCategoryDialog(activity, "サブカテゴリ", "カテゴリの追加はプラスボタンを押してください", dialogActions)
     }
@@ -88,20 +105,21 @@ class CategoryDialog(private val activity: Activity) {
             "fix"
         else
             "flex"
-        println("fuck $type")
-        CoroutineScope(Dispatchers.IO).async {
-            val mainCategoryList = app.db.loadMainCategory(type)
-            println("fuck2 "+mainCategoryList.size)
-            mainCategoryList.forEach { mainCategory ->
-                dialogActions.add(DialogAction(mainCategory.main_category_name, Runnable {
-                    //액티비티에 네임 보내기 필요에 따라서 id도
-                    // 콜 서브 카테고리 다이얼 로크
-                }))
-                println("fuck3 "+mainCategory.main_category_name)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            runBlocking {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val mainCategoryList = app.db.loadMainCategory(type)
+                    mainCategoryList.forEach { mainCategory ->
+                        dialogActions.add(DialogAction(mainCategory.main_category_name, Runnable {
+                            //액티비티에 네임 보내기 필요에 따라서 id도
+                            // 콜 서브 카테고리 다이얼 로크
+                        }))
+                    }
+                }.join()
+                displayCategoryDialog(activity, "メインカテゴリ", "メインカテゴリを選択してください。", dialogActions)
+
             }
-            println("fuck4 ")
-            displayCategoryDialog(activity, "メインカテゴリ", "メインカテゴリを選択してください。", dialogActions)
-            println("fuck5 ")
         }
     }
 

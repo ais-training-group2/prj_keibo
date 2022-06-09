5cfa3742ce085523f8e68b82f453a2a590356a7c
Ko DongHyeop <dkdhqt12@gmail.com>
2022-06-09 14:05:51 +0900

    -확장리스트 데이터 적용 -엔화원화 전환 버튼 처리

--- app/src/main/java/com/jp_ais_training/keibo/frament/CircleStatisticsFragment.kt
+++ app/src/main/java/com/jp_ais_training/keibo/frament/CircleStatisticsFragment.kt
@@ -1,13 +1,10 @@
 package com.jp_ais_training.keibo.frament
 
-import android.R
 import android.graphics.Color
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SimpleExpandableListAdapter
 import androidx.fragment.app.Fragment
 import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.PieData
@@ -15,6 +12,7 @@ import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.jp_ais_training.keibo.KeiboApplication
+import com.jp_ais_training.keibo.adapter.ExpandableListAdapter
 import com.jp_ais_training.keibo.databinding.FragmentCircleStatisticsBinding
 import kotlinx.coroutines.*
 import java.text.DateFormat
@@ -41,13 +39,28 @@ class CircleStatisticsFragment : Fragment() {
     private var job: Job = Job()
 
     //문자열 취득용 변수
-    var mainCategoryName = ""
+    private var mainCategoryName = ""
     private var mainSumBundle = ""
+    private var subCategoryName = ""
+    private var subSumBundle = ""
 
-    // 부모 리스트
-    private val groupData: ArrayList<HashMap<String, String?>> = ArrayList()
-    // 자식 리스트
-    private val childData: ArrayList<ArrayList<HashMap<String, String?>>> = ArrayList()
+    //확장리스트 데이터 클래스
+    data class MenuTitle(var title: String, var price: String, var index: Int)
+    data class MenuSpecific(var title: String, var detail: String?)
+
+    //확장리스트 변수
+    private var parentList = mutableListOf<MenuTitle>()
+    var childValueList = mutableListOf<MenuSpecific>()
+    private var childList = mutableListOf<MutableList<MenuSpecific>>()
+
+    //대분류 항목 리스트
+    private val nameSetMainCategory = arrayOf(
+        "公課金,","生活(固定),","その他(固定),",
+        "食費,","生活(変動),","余暇,",
+        "文化,","自己開発,","その他(変動)"
+    )
+    //최초 0  -> 엔화
+    var setJpOrKr = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -72,15 +85,14 @@ class CircleStatisticsFragment : Fragment() {
 
         //최초 기간 표시
         binding.circleKikanTv.setText(
-            df.format(cal.time).substring(0,4) + "年" + df.format(cal.time).substring(5,7)+"月01日 ~ "
-                    + df.format(cal.time).substring(8,10) +"日"
+            df.format(cal.time).substring(0,4) + "年" + df.format(cal.time).substring(5,7)+"月"
         )
 
         //다음달 통계 표시 버튼 최초 숨김
         binding.naviYokugetsuBtn.visibility = View.INVISIBLE
         binding.naviYokugetsuBtn.isEnabled = false
 
-        pickOutMainPrice(df.format(cal.time).substring(0,7))
+        getDataFromDB(df.format(cal.time).substring(0,7))
 
         //버튼 클릭 이벤트 -----------------------------------------------------------------------------------------
         //전달 통계 표시 버튼
@@ -88,7 +100,10 @@ class CircleStatisticsFragment : Fragment() {
             //실행중인 코루틴 취소
             job.cancel()
 
+            //DB 데이터 초기화
             clearPieChart()
+            clearExpandableList()
+
             moveLastMonth()
 
             //전달 기간 표시
@@ -96,63 +111,93 @@ class CircleStatisticsFragment : Fragment() {
             binding.circleKikanTv.setText(
                 df.format(cal.time).substring(0,4) + "年" + df.format(cal.time).substring(5,7) + "月"
             )
-            pickOutMainPrice(df.format(cal.time).substring(0,7))
+            getDataFromDB(df.format(cal.time).substring(0,7))
         }
         //다음달 통계 표시 버튼 -> dateStandard = 0이 될 경우 Invisible 처리
         binding.naviYokugetsuBtn.setOnClickListener(){
             //실행중인 코루틴 취소
             job.cancel()
 
+            //DB 데이터 초기화
             clearPieChart()
+            clearExpandableList()
+
             moveNextMonth()
 
             //다음달 기간 표시
             cal.add(Calendar.MONTH, 1)
             if(dateStandard == 0){
                 binding.circleKikanTv.setText(
-                    df.format(cal.time).substring(0,4) + "年" + df.format(cal.time).substring(5,7)+"月01日 ~ "
-                            + df.format(cal.time).substring(8,10) +"日"
+                    df.format(cal.time).substring(0,4) + "年" + df.format(cal.time).substring(5,7)+"月"
                 )
-                pickOutMainPrice(df.format(cal.time).substring(0,7))
-
+                getDataFromDB(df.format(cal.time).substring(0,7))
             }else{
                 binding.circleKikanTv.setText(
                     df.format(cal.time).substring(0,4) + "年" + df.format(cal.time).substring(5,7) + "月"
                 )
-                pickOutMainPrice(df.format(cal.time).substring(0,7))
+                getDataFromDB(df.format(cal.time).substring(0,7))
             }
         }
+        binding.tsukaKiriKaeBtn.setOnClickListener(){
+            if(setJpOrKr == 0){
+                //실행중인 코루틴 취소
+                job.cancel()
 
-        //확장리스트 세팅
-        setParent()
-        setChild()
-        setExListAdapter()
+                //DB 데이터 초기화
+                clearPieChart()
+                clearExpandableList()
+
+                //원화 설정
+                setJpOrKr = 1
+                ExpandableListAdapter.setKRW = false
+                binding.tsukaKiriKaeBtn.text = "￦"
+
+                getDataFromDB(df.format(cal.time).substring(0,7))
+
+            }else{
+                //실행중인 코루틴 취소
+                job.cancel()
+
+                //DB 데이터 초기화
+                clearPieChart()
+                clearExpandableList()
+
+                //엔화설정
+                setJpOrKr = 0
+                ExpandableListAdapter.setKRW = true
+                binding.tsukaKiriKaeBtn.text = "円"
+
+                getDataFromDB(df.format(cal.time).substring(0,7))
+            }
+        }
 
         return binding.root
     }
 
     //원그래프 옵션 설정
     private fun setPieChartOption(){
-        pieChart.setUsePercentValues(true)
-        pieChart.description.isEnabled = false
-        pieChart.setExtraOffsets(5f, 10f, 5f, 5f)
-        pieChart.dragDecelerationFrictionCoef = 0.95f
-        pieChart.setEntryLabelTextSize(0f)
-        pieChart.isDrawHoleEnabled = false
-        pieChart.setHoleColor(Color.WHITE)
-        pieChart.transparentCircleRadius = 61f
-        pieChart.legend.isEnabled = true //하단 색항목 리스트
-        pieChart.animateXY(1000, 1000) //초기 애니메이션 설정
+        binding.root.post{
+            pieChart.setUsePercentValues(true)
+            pieChart.description.isEnabled = false
+            pieChart.setExtraOffsets(5f, 10f, 5f, 5f)
+            pieChart.dragDecelerationFrictionCoef = 0.95f
+            pieChart.setEntryLabelTextSize(0f)
+            pieChart.isDrawHoleEnabled = false
+            pieChart.setHoleColor(Color.WHITE)
+            pieChart.transparentCircleRadius = 61f
+            pieChart.legend.isEnabled = true //하단 색항목 리스트
+            pieChart.animateXY(500, 500) //초기 애니메이션 설정
+        }
     }
 
     //원그래프 항목 추가
-    private fun setPieChartItem(setPieItem: String) {
+    private fun setPieChartItem(setPieItem: String, mainCategoryName: String) {
         val arrPrice = setPieItem.split(",")
-        val arrName = mainCategoryName.split(",")
-        if(arrPrice.isNotEmpty()){
-            for (i in 0 until arrPrice.size-1){
-                yValues.add(PieEntry(arrPrice[i].toFloat(), arrName[i]))
-            }
+        var arrName = mainCategoryName.split(",")
+
+        for (i in 0 until arrName.size-1){
+            yValues.add(PieEntry(arrPrice[i].toFloat(), arrName[i]))
+
         }
     }
 
@@ -199,30 +244,37 @@ class CircleStatisticsFragment : Fragment() {
         }
     }
 
-    //메인 카테고리 기준 DB 결과 값 가격 추출  <--  "yyyy-mm"형식 날짜, 메인 카테고리 번호 입력
-    private fun pickOutMainPrice(setDate: String){
+    //메인 카테고리 기준 DB 결과 값 가격 추출  <--  "yyyy-mm"형식 날짜 입력
+    private fun getDataFromDB(setDate: String){
         //DB에서 데이터 가져오기
         job = CoroutineScope(Dispatchers.Main).launch{
-            //원그래프 옵션 설정
-            setPieChartOption()
             withContext(CoroutineScope(Dispatchers.IO).coroutineContext) {
+                //DB에서 데이터 취득
                 var getSumMainCategory = app.db.loadMonthSumMainCategoryEI(setDate)
-                //println(app.db.loadMonthSumSubCategoryEI(setDate))
+                var getSubCategory = app.db.loadMonthSumSubCategoryEI(setDate)
+
+                //문자열 취득 변수 초기화
+                mainCategoryName = ""
+                mainSumBundle = ""
+                subCategoryName = ""
+                subSumBundle = ""
+
+                //서브 카테고리 데이터 저장 배열
+                var arrSub : List<String>
 
                 //초기 string 변수 선언
                 var str_data = getSumMainCategory.toString()
+                var str_subData = getSubCategory.toString()
+
                 //공백 제거
                 str_data = str_data.replace(" ", "")
+                str_subData = str_subData.replace(" ", "")
 
+                //문자열 취득용
                 val nameSetOfDBData = arrayOf(
                     "公課金,main_id=1,type=fix","生活,main_id=2,type=fix","その他,main_id=3,type=fix",
                     "食費,main_id=4,type=flex","生活,main_id=5,type=flex","余暇,main_id=6,type=flex",
                     "文化,main_id=7,type=flex","自己開発,main_id=8,type=flex","その他,main_id=9,type=flex"
-                )
-                val nameSetMainCategory = arrayOf(
-                    "公課金,","生活,","その他,",
-                    "食費,","生活,","余暇,",
-                    "文化,","自己開発,","その他,"
                 )
 
                 //메인 카테고리명 취득
@@ -232,7 +284,7 @@ class CircleStatisticsFragment : Fragment() {
                     }
                 }
 
-                //특정 문자열 변경
+                //메인 카테고리 문자열 변경
                 str_data = str_data.replace("LoadSumMainCategoryEI(date=", "")
                 str_data = str_data.replace("$setDate,price=", "")
                 str_data = str_data.replace(",main_name=公課金,main_id=1,type=fix)", "")
@@ -248,87 +300,86 @@ class CircleStatisticsFragment : Fragment() {
                 str_data = str_data.replace("]", "")
                 mainSumBundle = str_data
 
-                //원그래프 항목, 데이터 설정
-                setPieChartItem(mainSumBundle)
+                //서브 카테고리 문자열 변경
+                str_subData = str_subData.replace(" ","")
+                str_subData = str_subData.replace("[","")
+                str_subData = str_subData.replace("]","")
+                str_subData = str_subData.replace("LoadSumSubCategoryEI","")
+                str_subData = str_subData.plus(",")
+                str_subData = str_subData.replace("(","")
+
+                arrSub = str_subData.split("),")
+
+                //원그래프 항목, 옵션, 데이터 설정
+                setPieChartItem(mainSumBundle, mainCategoryName)
+                setPieChartOption()
                 setPieChartDataSet()
+
+                //확장리스트 데이터 설정
+                setExpandableList(mainCategoryName, mainSumBundle, arrSub)
             }
         }
     }
 
-    //확장리스트 -----------------------------------------------------------------------------------
-    //ExpandableListView 부모리스트 설정
-    private fun setParent(){
-        val groupA: HashMap<String, String?> = HashMap()
-        groupA["group"] = "1"
-        val groupB: HashMap<String, String?> = HashMap()
-        groupB["group"] = "2"
+    //확장리스트 데이터 세팅
+    private fun setExpandableList(
+        getMainName:String,
+        getMainPrice:String,
+        setSub:List<String>
+    )
+    {
+        val arrMainName = getMainName.split(",")
+        val arrMainPrice = getMainPrice.split(",")
 
-        groupData.add(groupA)
-        groupData.add(groupB)
-        /*확장리스트 테스트
-        println("groupA : $groupA")
-        println("groupB : $groupB")
-        println("groupData : $groupData")
-         */
+        var k = 0
+        //대분류 세팅
+        while (k <= arrMainPrice.size-1){
+            parentList.add(MenuTitle(arrMainName[k], arrMainPrice[k], k))
+            k++
+        }
+
+        //소분류 항목명, 가격 설정
+        for(i in 1..9){
+            childValueList.clear()
+            for(j in 0 until setSub.size-1){
+                if(setSub[j].contains("main_id=$i")){
+                    var bundleArr = setSub[j].split(",")
+
+                    childValueList.add(
+                        MenuSpecific(
+                            bundleArr[2].replace("sub_name=",""),
+                            bundleArr[1].replace("price=","")
+                        )
+                    )
+                }
+            }
+            //소분류 세팅
+            if(childValueList.toMutableList().isNotEmpty()){
+                childList.add(childValueList.toMutableList())
+            }
+        }
+
+        //어댑터 세팅
+        var expandableListAdapter = ExpandableListAdapter(requireContext(), parentList.toMutableList(), childList.toMutableList())
+        binding.root.post{
+            binding.expandableListView.setAdapter(expandableListAdapter)
+        }
     }
 
-    //ExpandableListView 자식리스트 설정
-    private fun setChild(){
-        val childListA: ArrayList<HashMap<String, String?>> = ArrayList()
-
-        val childAA: HashMap<String, String?> = HashMap()
-        childAA["group"] = "1"
-        childAA["name"] = "a"
-        childListA.add(childAA)
-
-        val childAB: HashMap<String, String?> = HashMap()
-        childAB["group"] = "1"
-        childAB["name"] = "b"
-        childListA.add(childAB)
-
-        val childAC: HashMap<String, String?> = HashMap()
-        childAC["group"] = "1"
-        childAC["name"] = "c"
-        childListA.add(childAC)
-
-        childData.add(childListA)
-
-        /*확장리스트 테스트
-        println("childAA : $childAA")
-        println("childListA : $childListA")
-        println("childData : $childData")
-         */
+    //확장리스트 초기화
+    private fun clearExpandableList(){
+        binding.expandableListView.invalidate()
+        parentList.clear()
+        childList.clear()
+        childValueList.clear()
     }
-
-    //ExpandableListView 어댑터 설정
-    private fun setExListAdapter(){
-        val adapter = SimpleExpandableListAdapter(
-            context,
-            groupData,
-            R.layout.simple_expandable_list_item_1,
-            arrayOf("group"),
-            intArrayOf(R.id.text1),
-            childData,
-            R.layout.simple_expandable_list_item_2,
-            arrayOf("name", "group"),
-            intArrayOf(R.id.text1, R.id.text2)
-        )
-
-        //확장리스트 어댑터 설정
-        val listView = binding.circleExV
-        listView.setAdapter(adapter)
-    }
-
-    //--------------------------------------------------------------------------------------------
 
     override fun onDestroyView() {
         //원그래프 데이터 초기화
         clearPieChart()
 
-        //확장리스트 데이터 초기화
-        groupData.clear()
-        childData.clear()
-        binding.circleExV.invalidate()
+        //확장리스트 초기화
+        clearExpandableList()
 
         //월별 이동 처리 기준 초기화
         dateStandard = 0
